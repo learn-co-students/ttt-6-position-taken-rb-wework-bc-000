f60c8d0096a17af6858b7db56806f71f35e94246
Tomas Medina <tomas.medina@wework.com>
2018-02-11 20:12:34 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,10 @@
 # code your #position_taken? method here!
+def position_taken?(array,index)
+  if array[index] == "X"
+    true
+  elsif array[index] == "O"
+    true
+  else
+    false
+  end
+end
