c3e1d8f1ea396f289134bc6be7b7c22cd145e420
Angela A <ranran.wang@wework.com>
2018-02-17 21:30:26 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,9 @@
 # code your #position_taken? method here!
+
+
+def position_taken (board,index)
+  if board[index] == " "
+    puts "false"
+end 
+
+# Value ="x" means when the user don't enter a value, it will just default to X
