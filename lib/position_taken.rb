bf4ecf7816a890e2ca968da90436942a795134a9
brendanmfarrell <brendan.farrell@wework.com>
2018-02-19 17:54:08 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,13 @@
 # code your #position_taken? method here!
+
+def position_taken?(board, index)
+  if board[index] == " "
+    FALSE
+  elsif board[index] == ""
+    FALSE
+  elsif board[index] == nil
+    FALSE 
+  elsif board[index] == "X" || "O"
+    TRUE
+  end
+end
